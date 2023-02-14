85821c5c4a233f0cc3dc714c2642cfc2a182db76
Lepeng Z <zlepeng.ucf@gmail.com>
2023-02-14 21:39:19 +0000

    done insert lab

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,1 @@
-write your sql statement in a single line in the problem1.sql file.
+INSERT INTO song (title,artist) VALUES ('let it be','Beatles'),('Hotel California','Eagles'),('Kashmir','Led Zeppelin');
